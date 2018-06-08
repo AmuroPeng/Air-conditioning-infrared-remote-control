d159de31dde23f692d02ca59afa3fd3faf35464b
Amuro Peng <tiggerpeng1997@gmail.com>
2018-06-08 18:05:06 +0800

    debug06081805

---
+++ IR_ps/IR_ps.sdk/IR_sdk/Debug/src/IR_cs.d
@@ -0,0 +1,7 @@
+src/IR_cs.o src/IR_cs.d: ../src/IR_cs.c \
+ ../../IR_sdk_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../IR_sdk_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../../IR_sdk_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../IR_sdk_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
---
+++ IR_ps/IR_ps.sdk/IR_sdk/src/IR_cs.c
@@ -0,0 +1,8 @@
+/*
+ * IR_cs.c
+ *
+ *  Created on: 2018Äê6ÔÂ8ÈÕ
+ *      Author: Amuro
+ */
+#include"xparameters.h"
+
